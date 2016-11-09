684ee78350360b8915d636ff1c993ce10f85cd97
nachmo5 <moham3d.nachit@gmail.com>
2016-11-09 23:12:55 +0000

    beta get

--- src/apicarnetBundle/Controller/AdresseController.php
+++ src/apicarnetBundle/Controller/AdresseController.php
@@ -11,23 +11,67 @@ namespace apicarnetBundle\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-
+use apicarnetBundle\Entity\Adresse;
 class AdresseController extends FOSRestController
 {
 
+    public function getAdresseAction($id,$slug)
+    {
+        $adresse= $this->getOneOr404($id,$slug);
+        $data = $this->serializeAdresse($adresse);
+
+        $response = new Response(json_encode($data), 200);
+        $response->headers->set('Content-Type', 'application/json');
+        $programmerUrl =  $this->generateUrl('api_1_get_contact_adresse', array('id' => $id,'slug'=>$slug, '_format' => 'json'));
+        $response->headers->set('Location', $programmerUrl);
+
+        return $response;
+
+    }
 
     public function getAdressesAction($id)
     {
-        $response = new Response($id, 200);
+        $adresses= $this->getOr404($id);
+        $data = array('adresses' => array());
+
+        foreach ($adresses as $adresse) {
+            $data['adresses'][] = $this->serializeAdresse($adresse);
+        }
+
+
+        $response = new Response(json_encode($data), 200);
+        $response->headers->set('Content-Type', 'application/json');
+        $programmerUrl =  $this->generateUrl('api_1_get_contact_adresses', array('id' => $id, '_format' => 'json'));
+        $response->headers->set('Location', $programmerUrl);
+
         return $response;
 
     } // "get_contact_adresses"   [GET] /contacts/{id}/adresses
     protected function getOr404($id)
     {
-        if (!($adresse = $this->container->get('doctrine.orm.entity_manager')->getRepository('apicarnetBundle:Adresse')->find($id)  )) {
+        if (!($adresses = $this->container->get('doctrine.orm.entity_manager')->getRepository('apicarnetBundle:Adresse')->findBy(array('contact' => $id)) )) {
             throw new NotFoundHttpException(sprintf('Ressource \'%s\' introuvable.',$id));
         }
 
-        return $adresse;
+        return $adresses;
+    }
+    protected function getOneOr404($id,$slug)
+    {
+        if (!($adresses = $this->container->get('doctrine.orm.entity_manager')->getRepository('apicarnetBundle:Adresse')->find($slug) )) {
+            throw new NotFoundHttpException(sprintf('Ressource \'%s\' introuvable.',$slug));
+        }
+
+        return $adresses;
+    }
+
+    private function serializeAdresse(Adresse $programmer)
+    {
+        return array(
+            'code_postal' => $programmer->getCodePostal(),
+            'rue' => $programmer->getRue(),
+            'ville' => $programmer->getVille(),
+            'cree_le' => $programmer->getCreeLe(),
+            'misajour_le' => $programmer->getMisajourLe(),
+        );
     }
 }
--- src/apicarnetBundle/Controller/ContactController.php
+++ src/apicarnetBundle/Controller/ContactController.php
@@ -12,24 +12,35 @@ namespace apicarnetBundle\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-
+use apicarnetBundle\Entity\Contact;
 
 class ContactController extends FOSRestController
 {
+
+    public function getContactsAction()
+    {
+        $contacts= $this->getOr404();
+        $data = array('contacts' => array());
+
+        foreach ($contacts as $contact) {
+            $data['contacts'][] = $this->serializeContact($contact);
+        }
+
+        $response = new Response(json_encode($data), 200);
+        $response->headers->set('Content-Type', 'application/json');
+        $programmerUrl =  $this->generateUrl('api_1_get_contacts', array('_format' => 'json'));
+        $response->headers->set('Location', $programmerUrl);
+
+        return $response;
+
+    }
     public function getContactAction($id)
     {
 
-        $contact= $this->getOr404($id);
+        $contact= $this->getOneOr404($id);
 
 
-        $data = array(
-            'civilite' => $contact->getCivilite(),
-            'nom' => $contact->getNom(),
-            'prenom' => $contact->getPrenom(),
-            'date_de_naissance' => $contact->getDateDeNaissance(),
-            'cree_le' => $contact->getCreeLe(),
-            'misajour_le' => $contact->getMisajourLe(),
-        );
+        $data =$this->serializeContact($contact);
 
 
         $response = new Response(json_encode($data), 200);
@@ -41,12 +52,32 @@ class ContactController extends FOSRestController
 
     } // "get_contact"   [GET] /contacts/{id}
 
-    protected function getOr404($id)
+    protected function getOneOr404($id)
     {
         if (!($contact = $this->container->get('doctrine.orm.entity_manager')->getRepository('apicarnetBundle:Contact')->find($id)  )) {
             throw new NotFoundHttpException(sprintf('Ressource \'%s\' introuvable.',$id));
         }
 
         return $contact;
+    }
+    protected function getOr404()
+    {
+        if (!($contact = $this->container->get('doctrine.orm.entity_manager')->getRepository('apicarnetBundle:Contact')->findAll()  )) {
+            throw new NotFoundHttpException(sprintf('Ressource  introuvable.'));
+        }
+
+        return $contact;
+    }
+
+    private function serializeContact(Contact $contact)
+    {
+        return array(
+            'civilite' => $contact->getCivilite(),
+            'nom' => $contact->getNom(),
+            'prenom' => $contact->getPrenom(),
+            'date_de_naissance' => $contact->getDateDeNaissance(),
+            'cree_le' => $contact->getCreeLe(),
+            'misajour_le' => $contact->getMisajourLe(),
+        );
     }
 }
