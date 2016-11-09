d1ac9ba899b71a9e10d9c509fea52239018800a2
nachmo5 <moham3d.nachit@gmail.com>
2016-11-09 18:23:32 +0000

    reponse json

--- src/apicarnetBundle/Controller/ContactController.php
+++ src/apicarnetBundle/Controller/ContactController.php
@@ -11,6 +11,7 @@ namespace apicarnetBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 class ContactController extends FOSRestController
@@ -18,9 +19,33 @@ class ContactController extends FOSRestController
     public function getContactAction($id)
     {
 
-        $contact= $this->container->get('doctrine.orm.entity_manager')->getRepository('apicarnetBundle:Contact')->find($id);
-        $response=new Response($contact->getCivilite(),'200');
+        $contact= $this->getOr404($id);
+
+
+        $data = array(
+            'civilite' => $contact->getCivilite(),
+            'nom' => $contact->getNom(),
+            'prenom' => $contact->getPrenom(),
+            'date_de_naissance' => $contact->getDateDeNaissance(),
+            'cree_le' => $contact->getCreeLe(),
+            'misajour_le' => $contact->getMisajourLe(),
+        );
+
+
+        $response = new Response(json_encode($data), 200);
+        $response->headers->set('Content-Type', 'application/json');
+        $programmerUrl =  $this->generateUrl('api_1_get_contact', array('id' => $contact->getId(), '_format' => 'json'));
+        $response->headers->set('Location', $programmerUrl);
+
         return $response;
 
+    }
+    protected function getOr404($id)
+    {
+        if (!($contact = $this->container->get('doctrine.orm.entity_manager')->getRepository('apicarnetBundle:Contact')->find($id)  )) {
+            throw new NotFoundHttpException(sprintf('Ressource \'%s\' introuvable.',$id));
+        }
+
+        return $contact;
     }
 }
